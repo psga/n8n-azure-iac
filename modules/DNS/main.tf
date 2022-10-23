68cf0e2f6522573588a1901bdfb06224d97979cc
psga <pusugag@unal.edu.co>
2022-10-23 10:43:53 -0500

    change name

--- modules/DNS/main.tf
+++ modules/DNS/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_dns_zone" "main" {
-  name                = "tu-dominio.com" # El que compraste en Namecheap
+  name                = "pusuga.me" # El que compraste en Namecheap
   resource_group_name = var.resource_group_name
 }
